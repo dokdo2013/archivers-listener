3390d4e9a2604d8b1459049ca3a81da2a3a0232c
hyeonwoojo <h_jo@ejn.gg>
2023-10-02 00:30:42 +0900

    feat: add root endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeController } from '@nestjs/swagger';
 
 @Controller()
+@ApiExcludeController()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('')
+  async getHello() {
+    return this.appService.getHello();
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -28,6 +28,7 @@ import { VideoModule } from './video/video.module';
       redis: {
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT),
+        password: process.env.REDIS_PASSWORD || undefined,
       },
     }),
 
